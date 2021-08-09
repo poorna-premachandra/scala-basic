dbf56912ee54931cd334edc0062ac83941fc2092
Poorna <17600522+Poorna14@users.noreply.github.com>
2021-08-09 12:21:37 +0530

    custom "Set" implementation
    Signed-off-by: Poorna <17600522+Poorna14@users.noreply.github.com>

---
+++ src/exercises/part2oop/MySet.scala
@@ -0,0 +1,66 @@
+package exercises.part2oop
+
+import scala.annotation.tailrec
+
+//let's prove  Set is a function
+trait MySet[A] extends (A => Boolean) { //(A => Boolean) is the apply method?
+
+  def apply(elem: A): Boolean = contains(elem)
+  def contains(elem: A): Boolean
+  def +(elem: A): MySet[A]
+  def ++(anotherSet: MySet[A]): MySet[A]
+
+  def map[B](f: A => B): MySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B]
+  def filter(predicate: A => Boolean): MySet[A]
+  def foreach(f: A => Unit): Unit
+}
+
+class EmptySet[A] extends MySet[A] { //defined a class not a object because MySet[A] is in-variance
+  def contains(elem: A): Boolean = false
+  def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
+  def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
+
+  def map[B](f: A => B): MySet[B] = new EmptySet[B]
+  def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
+  def filter(predicate: A => Boolean): MySet[A] = this
+  def foreach(f: A => Unit): Unit = ()
+}
+
+
+class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
+  def contains(elem: A): Boolean =
+    elem == head || tail.contains(elem)
+
+  def +(elem: A): MySet[A] =
+    if (this contains elem) this
+    else new NonEmptySet[A](elem, this)
+
+  def ++(anotherSet: MySet[A]): MySet[A] =
+    tail ++ anotherSet + head
+
+  def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
+  def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
+  def filter(predicate: A => Boolean): MySet[A] = {
+    val filteredTail = tail filter predicate
+    if (predicate(head)) filteredTail + head
+    else filteredTail
+  }
+
+  def foreach(f: A => Unit): Unit = {
+    f(head)
+    tail foreach f
+  }
+}
+
+
+object MySet {
+  def apply[A](values: A*): MySet[A] = {
+    @tailrec
+    def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
+      if (valSeq.isEmpty) acc
+      else buildSet(valSeq.tail, acc + valSeq.head)
+
+    buildSet(values.toSeq, new EmptySet[A])
+  }
+}
