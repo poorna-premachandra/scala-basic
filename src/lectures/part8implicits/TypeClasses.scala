0f8abcc976ff062b2f2d816cf2589f338fbfca78
Poorna <17600522+Poorna14@users.noreply.github.com>
2021-09-22 21:33:48 +0530

    include type class lecture 2

--- src/lectures/part8implicits/TypeClasses.scala
+++ src/lectures/part8implicits/TypeClasses.scala
@@ -25,7 +25,7 @@ object TypeClasses extends App {
     def apply(value1: T, value2: T): Boolean
   }
 
-  object CompareByName extends Equal[User] {
+  implicit object CompareByName extends Equal[User] {
     override def apply(user1: User, user2: User): Boolean = user1.name.equals(user2.name)
   }
 
@@ -37,4 +37,32 @@ object TypeClasses extends App {
 
   println(CompareByName(john, mary))
   println(CompareByName(john, john))
+
+
+  object HTMLSerializer {
+    def serialize[T](value: T)(implicit serializer: HTMLSerializer[T]): String = serializer.serialize(value)
+
+    def apply[T](implicit serializer: HTMLSerializer[T]) = serializer //better alternative
+  }
+
+  implicit object StringSerializer extends HTMLSerializer[String] {
+    override def serialize(value: String): String = s"<div>Serialized String : $value </div>"
+  }
+
+  implicit object IntSerializer extends HTMLSerializer[Int] {
+    override def serialize(value: Int): String = s"<div>Serialized Int : $value</div>"
+  }
+
+  println(HTMLSerializer.serialize(42))
+  println(HTMLSerializer.serialize("bob"))
+
+  //better alternative - have access to the type class
+  println(HTMLSerializer[Int].serialize(42))
+
+  //EXERCISE : implement TC(Type Class) pattern for Equality example
+  object Equal {
+    def apply[T](value1: T, value2: T)(implicit equalizer: Equal[T]): Boolean = equalizer.apply(value1, value2) //make only one method implicit from the above two methods
+  }
+  println(Equal(john, mary))
+  // This is called AD-HOC polymorphism
 }
